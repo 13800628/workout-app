8db3678eab51f8a08c6e76ea63dc7e9c181bb597
Yuto Isayama <isayamayuto@icloud.com>
2026-02-08 21:18:04 +0900

    クエリの短縮

--- src/main/java/com/workout/repository/UserRepository.java
+++ src/main/java/com/workout/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.workout.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.workout.model.User;
@@ -8,6 +11,9 @@ import com.workout.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
   
+  @Modifying
+  @Query("DELETE FROM User u WHERE u.id = :id")
+  int deleteDirectlyById(@Param("id") Long id);
 }
 
 
