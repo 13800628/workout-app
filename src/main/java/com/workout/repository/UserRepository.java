28ab7a47a3251c74f790db3c6b5f6c8f936e261b
Yuto Isayama <isayamayuto@icloud.com>
2026-02-11 21:05:50 +0900

    spring securityの形だけ追加

--- src/main/java/com/workout/repository/UserRepository.java
+++ src/main/java/com/workout/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.workout.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,6 +16,8 @@ public interface UserRepository extends JpaRepository<User, Long>{
   @Modifying
   @Query("DELETE FROM User u WHERE u.id = :id")
   int deleteDirectlyById(@Param("id") Long id);
+
+  Optional<User> findByUsername(String username);
 }
 
 
