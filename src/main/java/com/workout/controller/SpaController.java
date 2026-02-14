5d8381e7da39fe8e854ef5dd453f2c45b9101f09
Yuto Isayama <isayamayuto@icloud.com>
2026-02-14 16:00:11 +0900

    ワークアウトページ設定

--- src/main/java/com/workout/controller/SpaController.java
+++ src/main/java/com/workout/controller/SpaController.java
@@ -6,16 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SpaController {
 
-    // API (/api/**) でも、静的ファイル (.js, .css, .pngなど) でもないパスに来たら
-    // 全て index.html を返す
-    @GetMapping(value = "{path:[^\\.]*}")
-    public String redirect() {
-        return "forward:/index.html";
-    }
-
-    // 2階層以上のパス (/workout/details など) にも対応
-    @GetMapping(value = "/**/{path:[^\\.]*}")
-    public String redirectDeep() {
+    // API (/api) でも 静的ファイル (.js, .css など) でもない
+    // "/workout" や "/workout/12" といったリクエストを全て index.html にフォワードする
+    @GetMapping({"/workout", "/workout/**"})
+    public String forward() {
         return "forward:/index.html";
     }
 }
