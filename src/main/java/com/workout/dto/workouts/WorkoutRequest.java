e67d299080961c666472f3f892eba892af98b55d
Yuto Isayama <isayamayuto@icloud.com>
2026-07-07 13:31:20 +0900

    アノテーション追加

--- src/main/java/com/workout/dto/workouts/WorkoutRequest.java
+++ src/main/java/com/workout/dto/workouts/WorkoutRequest.java
@@ -10,15 +10,18 @@ public record WorkoutRequest(
   @NotBlank(message = "種目名は必須です")
   String name,
 
+  @NotNull(message = "回数は必須です")
   @Min(0)
   Integer reps,
 
+  @NotNull(message = "セット数は必須です")
   @Min(0)
   Integer sets,
   
+  @NotNull(message = "重量は必須です")
   @Min(0)
   Integer weights,
 
-  @NotNull
+  @NotNull(message = "ユーザーIDは必須です")
   Long userId
 ) {}
