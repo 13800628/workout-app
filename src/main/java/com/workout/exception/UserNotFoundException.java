a7f1959a7f0b5c50920376183d1e7537b108fabf
Yuto Isayama <isayamayuto@icloud.com>
2026-07-10 20:19:08 +0900

    コメント追加

--- src/main/java/com/workout/exception/UserNotFoundException.java
+++ src/main/java/com/workout/exception/UserNotFoundException.java
@@ -1,5 +1,6 @@
 package com.workout.exception;
 
+// 浅いモジュールになっているので、もっと抽象的な例外クラスを作りこれを統合する方向でいく
 public class UserNotFoundException extends RuntimeException {
   
   public UserNotFoundException(String message) {
