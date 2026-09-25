86b8e00b40b9924ee3a56b8d23a6605e05630af4
Yuto Isayama <isayamayuto@icloud.com>
2026-09-25 19:02:56 +0900

    修正

--- src/test/java/com/workout/service/WorkoutServiceTest.java
+++ src/test/java/com/workout/service/WorkoutServiceTest.java
@@ -3,7 +3,6 @@ package com.workout.service;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.*;
-import static org.mockito.Mockito.*;
 
 import java.util.List;
 import java.util.Optional;
